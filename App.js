c682da0f7780fdbfc1a13c4ae86aa9b51439c89c
Firdous Ali <firdous.ali.tp@gmail.com>
2023-12-20 12:26:11 +0500

    clearup

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {SafeAreaView, View, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1}}>
+        <Text>test string</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
