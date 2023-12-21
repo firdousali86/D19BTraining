17185b9b42476e83e5a4461e23791f6bca375b36
Firdous Ali <firdous.ali.tp@gmail.com>
2023-12-21 12:45:04 +0500

    form element in basic info

--- src/components/ClassComponents/UserBasicInfo.js
+++ src/components/ClassComponents/UserBasicInfo.js
@@ -2,12 +2,33 @@ import React from 'react';
 import {View, Text, TextInput} from 'react-native';
 
 class UserBasicInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: props.basicInfo?.firstName,
+    };
+  }
+
   render() {
     const {basicInfo} = this.props;
 
+    const {firstName} = this.state;
+
     return (
       <View>
-        <Text>First Name: {basicInfo?.firstName}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text>First Name: </Text>
+          <TextInput
+            style={{backgroundColor: 'grey', marginHorizontal: 10, flex: 1}}
+            placeholder="First Name"
+            value={firstName}
+            onChangeText={changedText => {
+              this.setState({firstName: changedText});
+            }}
+          />
+        </View>
+
         <Text>Last Name: {basicInfo?.lastName}</Text>
         <Text>Gender: {basicInfo?.gender}</Text>
         <Text>DOB: {basicInfo?.DOB}</Text>
