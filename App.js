93270e1fdd19bb67e2c218123579748732a7173f
Prakash Malviya <er.prakashmalviya@gmail.com>
2023-12-26 10:25:52 +0530

    First Assignment

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {FunAndClass, FirstAssignment,Details} from './src/components/master/Index';
+import {FunAndClass, FirstAssignment, Details} from './src/components/master/Index';
 
 const Stack = createNativeStackNavigator();
+
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="First" component={FirstAssignment} />
-        <Stack.Screen name="Home" component={FunAndClass} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Weekly First Assignment" component={FirstAssignment}/>
+                <Stack.Screen name="Home" component={FunAndClass}/>
+                <Stack.Screen name="Details" component={Details}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default App;
