e59ce669fd64b85cb23575223093c8ac09207b46
Firdous Ali <firdous.ali.tp@gmail.com>
2023-12-27 11:58:02 +0500

    splitting stacks

--- src/navigate/index.js
+++ src/navigate/index.js
@@ -12,28 +12,54 @@ import {
   DetailsScreen,
   TypeScriptTestScreen,
 } from '../containers';
+import {Text, View} from 'react-native';
 
 const Navigation = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="TypeScriptTestScreen"
-        component={TypeScriptTestScreen}
-      />
-      <Stack.Screen name="ListScreen" component={ListScreen} />
-      <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
-      <Stack.Screen name="Dashboard" component={DashboardScreen} />
-      <Stack.Screen name="TestFlexScreen" component={TestFlexScreen} />
+  const getAuthStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen
+          name={'Login'}
+          component={() => (
+            <View>
+              <Text>this is login screen</Text>
+            </View>
+          )}></Stack.Screen>
+        <Stack.Screen
+          name={'Signup'}
+          component={() => (
+            <View>
+              <Text>this is signup screen</Text>
+            </View>
+          )}></Stack.Screen>
+      </Stack.Group>
+    );
+  };
 
-      <Stack.Screen name="TestHOCScreen" component={TestHOCScreen} />
+  const getMainStack = () => {
+    return (
+      <Stack.Group>
+        <Stack.Screen
+          name="TypeScriptTestScreen"
+          component={TypeScriptTestScreen}
+        />
+        <Stack.Screen name="ListScreen" component={ListScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="TestFlexScreen" component={TestFlexScreen} />
 
-      <Stack.Screen
-        name="Settings"
-        component={SettingsScreen}
-        initialParams={{city: 'London', country: 'UK'}}
-      />
-    </Stack.Navigator>
-  );
+        <Stack.Screen name="TestHOCScreen" component={TestHOCScreen} />
+
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          initialParams={{city: 'London', country: 'UK'}}
+        />
+      </Stack.Group>
+    );
+  };
+
+  return <Stack.Navigator>{getAuthStack()}</Stack.Navigator>;
 };
 
 export default Navigation;
