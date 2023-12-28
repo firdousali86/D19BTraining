3d31d2761ac097a11db6a4423e862402fd1d1b8b
Naresh <naresh.it95@gmail.com>
2023-12-28 09:33:46 +0530

    drawernavigation

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import 'react-native-gesture-handler';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MyTab from './src/components/TabNavigation/Tab';
+import MyDrawer from './src/components/Drawer/Drawer';
 
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTab />
+      <MyDrawer />
     </NavigationContainer>
   );
 }
--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Drawer = createDrawerNavigator();
