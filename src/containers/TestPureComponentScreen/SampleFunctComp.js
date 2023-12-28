9eadd0090bdb51e75c3fa89daa720490d0cebcc2
Firdous Ali <firdous.ali.tp@gmail.com>
2023-12-28 12:30:48 +0500

    useeffect usecases

--- src/containers/TestPureComponentScreen/SampleFunctComp.js
+++ src/containers/TestPureComponentScreen/SampleFunctComp.js
@@ -1,8 +1,20 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {memo, useEffect} from 'react';
 
-const SampleFunctComp = () => {
+const SampleFunctComp = props => {
   console.log('child func comp rerendered');
+
+  useEffect(() => {
+    console.log('componentdidmount of child func component');
+  }, []);
+
+  useEffect(() => {
+    console.log('entered text has changed');
+
+    return () => {
+      //
+    };
+  }, [props.enteredText]);
 
   return (
     <View>
