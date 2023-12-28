55a9483d9309b4c587a2739022f6ab4fa666f8af
Firdous Ali <firdous.ali.tp@gmail.com>
2023-12-28 11:48:47 +0500

    adding state on decision making

--- src/containers/TestPureComponentScreen/SampleClassComp.js
+++ src/containers/TestPureComponentScreen/SampleClassComp.js
@@ -1,13 +1,21 @@
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 import React, {Component} from 'react';
 import _ from 'lodash';
 
 export class SampleClassComp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {datetime: undefined};
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
-    // console.log(nextProps);
+    console.log('should component update called');
 
     // return nextProps.enteredText !== this.props.enteredText;
-    return !_.isEqual(this.props, nextProps);
+    return (
+      !_.isEqual(this.props, nextProps) || !_.isEqual(this.state, nextState)
+    );
   }
 
   //   componentDidUpdate() {
@@ -23,6 +31,14 @@ export class SampleClassComp extends Component {
       <View>
         <Text>this is very simple class comp</Text>
         <Text>{enteredText}</Text>
+        <Text>{this.state.datetime}</Text>
+
+        <Button
+          title={'Update date'}
+          onPress={() => {
+            this.setState({datetime: Date()});
+          }}
+        />
       </View>
     );
   }
