a2751bf46191a56448efecd59b3fddf7cefb297b
Firdous Ali <firdous.ali.tp@gmail.com>
2023-12-28 12:43:16 +0500

    breaking chain of renders

--- src/containers/TestPureComponentScreen/SampleFunctComp.js
+++ src/containers/TestPureComponentScreen/SampleFunctComp.js
@@ -1,5 +1,27 @@
 import {View, Text} from 'react-native';
 import React, {memo, useEffect} from 'react';
+import UserBasicInfo from '../../components/ClassComponents/UserBasicInfo';
+import UserContactInfo from '../../components/ClassComponents/UserContactInfo';
+
+const userObject = {
+  basicInfo: {
+    firstName: 'Firdous',
+    lastName: 'Ali',
+    gender: 'Male',
+    DOB: 'Dec 3',
+  },
+  contactInfo: {
+    cell: '123456789',
+    email: 'firdous@firdous.com',
+    address: '123 street',
+    city: 'Karachi',
+    country: 'Pakistan',
+  },
+  educationInfo: {
+    lastDegree: 'MS',
+    university: 'NEDUET',
+  },
+};
 
 const SampleFunctComp = props => {
   console.log('child func comp rerendered');
@@ -19,8 +41,10 @@ const SampleFunctComp = props => {
   return (
     <View>
       <Text>this is very simple functional comp</Text>
+      <UserBasicInfo basicInfo={userObject.basicInfo} />
+      <UserContactInfo contactInfo={userObject.contactInfo} />
     </View>
   );
 };
 
-export default SampleFunctComp;
+export default memo(SampleFunctComp);
