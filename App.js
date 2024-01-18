6dffffbd24146ce82b9440a35e92e271e821bdca
Firdous Ali <firdous.ali.tp@gmail.com>
2024-01-18 11:54:24 +0500

    usage of usebeforelift

--- App.js
+++ App.js
@@ -5,17 +5,22 @@
  * @format
  */
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './src/navigate';
-import {LogBox} from 'react-native';
+import {LogBox, Text, View} from 'react-native';
 import ErrorBoundary from './src/components/ErrorBoundary';
-import {store} from './src/store';
+import {store, persistor} from './src/store';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {DataHelper} from './src/helpers';
+
 // import {UserContextProvider} from './src/contexts/UserContext';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
   LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
   LogBox.ignoreAllLogs();
 
@@ -23,13 +28,32 @@ function App() {
 
   // console.log(isUserLoggedIn);
 
+  useEffect(() => {
+    if (store && !isLoading) {
+      DataHelper.setStore(store);
+    }
+  }, [isLoading]);
+
   return (
     <Provider store={store}>
-      <ErrorBoundary>
-        <NavigationContainer>
-          <Navigation />
-        </NavigationContainer>
-      </ErrorBoundary>
+      <PersistGate
+        persistor={persistor}
+        onBeforeLift={() => {
+          console.log('store is loaded');
+          // DataHelper.setStore(store);
+
+          setIsLoading(false);
+        }}>
+        <ErrorBoundary>
+          {isLoading ? (
+            <Text>Loading...</Text>
+          ) : (
+            <NavigationContainer>
+              <Navigation />
+            </NavigationContainer>
+          )}
+        </ErrorBoundary>
+      </PersistGate>
     </Provider>
   );
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import PersistanceHelper from './PersistanceHelper';
 import ApiHelper from './ApiHelper';
+import DataHelper from './DataHelper';
 
-export {PersistanceHelper, ApiHelper};
+export {PersistanceHelper, ApiHelper, DataHelper};
