413a44252b72b3d76ea45f018286c1d0e213da0a
Firdous Ali <firdous.ali.tp@gmail.com>
2024-01-22 10:35:39 +0500

    ios build fix

--- src/store.js
+++ src/store.js
@@ -1,16 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit';
-
 import {createLogger} from 'redux-logger';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import reducers from './features/reducers';
+
 import {combineReducers} from '@reduxjs/toolkit';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
 import persistStore from 'redux-persist/lib/persistStore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import reducers from './features/reducers';
 
 const isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
 
 let persistConfig = {key: 'root', storage: AsyncStorage};
+
 let rootReducer = combineReducers(reducers);
 
 let persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -22,17 +23,9 @@ const logger = createLogger({
   diff: true,
 });
 
-const middlewares = [];
-
-if (__DEV__) {
-  const createDebugger = require('redux-flipper').default;
-  middlewares.push(createDebugger());
-}
-
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(middlewares),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 export const persistor = persistStore(store);
