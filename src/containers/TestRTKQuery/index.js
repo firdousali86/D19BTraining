62f09ba47c9cdaa15ce600347b3fc8b289397283
Firdous Ali <firdous.ali.tp@gmail.com>
2024-02-14 11:20:59 +0500

    rtk query mutation successful

--- src/containers/TestRTKQuery/index.js
+++ src/containers/TestRTKQuery/index.js
@@ -1,9 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {useGetAllItemsQuery} from '../../services/itemApi';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TextInput,
+} from 'react-native';
+import React, {useState} from 'react';
+import {useGetAllItemsQuery, usePostItemMutation} from '../../services/itemApi';
 
 const TestRTKQuery = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
+
   const {data, error, isLoading} = useGetAllItemsQuery();
+  const [postItem, {isLoading: isPosting}] = usePostItemMutation();
 
   console.log(data);
 
@@ -11,6 +23,35 @@ const TestRTKQuery = () => {
     <View>
       <Text>index</Text>
       {isLoading && <Text>Loading</Text>}
+
+      <TextInput
+        value={title}
+        onChangeText={ct => {
+          setTitle(ct);
+        }}
+        placeholder="Title"
+      />
+      <TextInput
+        value={description}
+        onChangeText={ct => {
+          setDescription(ct);
+        }}
+        placeholder="Title"
+      />
+      <TextInput
+        value={image}
+        onChangeText={ct => {
+          setImage(ct);
+        }}
+        placeholder="Title"
+      />
+
+      <Button
+        title={'Post item'}
+        onPress={() => {
+          postItem({title, description, image});
+        }}
+      />
     </View>
   );
 };
--- src/services/itemApi.js
+++ src/services/itemApi.js
@@ -3,13 +3,28 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const itemApi = createApi({
   reducerPath: 'itemApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://jsonplaceholder.typicode.com/',
+    baseUrl: 'http://localhost:3000/api/',
   }),
   endpoints: builder => ({
     getAllItems: builder.query({
-      query: name => 'todos',
+      query: name => 'items',
+    }),
+    postItem: builder.mutation({
+      query: newItem => ({
+        url: 'items',
+        method: 'POST',
+        body: {
+          title: newItem.title,
+          image: newItem.image,
+          details: newItem.description,
+        },
+        headers: {
+          'X-Access-Token':
+            'ZopHp4TC4ousbqoVSV3uZYFgBszCowcx5jrwRFqGe5BX1Cnb8qbV8aGUpssDFlrh',
+        },
+      }),
     }),
   }),
 });
 
-export const {useGetAllItemsQuery} = itemApi;
+export const {useGetAllItemsQuery, usePostItemMutation} = itemApi;
